4fc0ab044dd21752b29a89b6814d580a9ce788ad
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2023-05-29 19:59:41 +0900

    Update and rename status-test.py to status.py

--- cogs/status.py
+++ cogs/status.py
@@ -1,23 +1,23 @@
 from discord.ext import commands
 from discord import app_commands
 import discord
-from ping3 import ping as pinge
+from ping3 import ping as ping
 import re
 
-class ping(commands.Cog):
+class status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name="status", description="サーバーの状態を表示します")
     @app_commands.guilds(1111683749969657938)
-    async def ping(self, interaction:discord.Interaction,):
+    async def status(self, interaction:discord.Interaction,):
             target = 'google.com'
 
-            result = int(pinge(target, unit="ms"))
+            result = int(ping(target, unit="ms"))
             if int(result) <= 0:
                 await interaction.response.send_message("失敗しました。", ephemeral=True)
-            interaction.response.send_message(embed=discord.Embed(title=f'"{target}"へのping',description=str(result) + "ms", color=discord.Colour.from_rgb(128,255,0)))
+            await interaction.response.send_message(embed=discord.Embed(title=f'"{target}"へのping',description=str(result) + "ms", color=discord.Colour.from_rgb(128,255,0)))
 
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(ping(bot))
+    await bot.add_cog(status(bot))
