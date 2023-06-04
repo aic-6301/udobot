7827e6b306931f07398ca0bf2110edec3bcb528b
aic-6301 <rx435423@gmail.com>
2023-06-04 13:28:16 +0900

    災害情報送信機能 Fixes #16

--- cogs/jma.py
+++ cogs/jma.py
@@ -7,15 +7,34 @@ import requests
 class jma(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.jma_get.start()
 
     @tasks.loop(minutes=1)
     async def jma_get(self):
+        with open("/data/data.json", "r") as f:
+            id = json.load(f)["id"]
+            print(id)
         response = requests.get('https://www.data.jma.go.jp/developer/xml/feed/extra.xml')
-        to_json = xmltodict.parse(response.text)
-        data = json.dumps(to_json, encoding="utf-8", ensure_ascii=False)
-        with open("./data/data.json", "w", encoding="utf-8") as f:
-            f.write(data)
+        if response.status_code == 200:
+            to_json = xmltodict.parse(response.text)
+            data = json.dumps(to_json,  ensure_ascii=False)
+            if data["entry"][0]["id"] != id["id"]:
+                print(data)
+                embed = discord.Embed(title=data["entry"][0]["title"], description=data["entry"][0]["content"]["#text"], color=0x00ff00)
+                await self.bot.get_channel("1114354017477345330").send(embed=embed)
+                with open("/data/data.json", "w") as f:
+                    json_data = json.loads(f)
+                    json_data["id"] = data["entry"][0]["id"]
+            else:
+                return
+        else:
+            return
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.jma_get.start()
+    
+    async def cog_unload(self):
+        self.jma_get.stop()
 
 async def setup(bot):
     await bot.add_cog(jma(bot))
