532e5d175f800d70bb74240fafe73cb9a294f88d
aic-6301 <rx435423@gmail.com>
2023-06-04 13:43:31 +0900

    a

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 import traceback
 from motor import motor_asyncio as motor
-import earthquake
 
 import discord
 from discord import Activity, ActivityType, Intents
@@ -36,7 +35,6 @@ class Bot(BotBase):
         await self.load_extension("jishaku") # jishakuをロード
         await self.change_presence(activity=discord.Game("色んな人によるこの鯖だけのぼっと"))
         print("起動したよ！！！")
-        earthquake.run()
 
 
 #実行する場所
