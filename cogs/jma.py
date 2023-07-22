dd3c61afa3b902f26a9e23a5fb6d9e331ff18116
aic-6301 <rx435423@gmail.com>
2023-07-22 23:10:22 +0900

    コメントつける

--- cogs/jma.py
+++ cogs/jma.py
@@ -8,20 +8,21 @@ class jma(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.jma_get.start()
+
     @tasks.loop(minutes=1)
     async def jma_get(self):
-        with open("/data/data.json", "r") as f:
+        with open("/data/data.json", "r") as f: # idを取得
             id = json.load(f)["id"]
             print(id)
         response = requests.get('https://www.data.jma.go.jp/developer/xml/feed/extra.xml')
         if response.status_code == 200:
-            to_json = xmltodict.parse(response.text)
-            data = json.dumps(to_json,  ensure_ascii=False)
-            if data["entry"][0]["id"] != id["id"]:
+            to_json = xmltodict.parse(response.text) # xmlをパース
+            data = json.dumps(to_json,  ensure_ascii=False) # jsonに変換(文字化けしないように)
+            if data["entry"][0]["id"] != id["id"]: # id照合
                 print(data)
                 embed = discord.Embed(title=data["entry"][0]["title"], description=data["entry"][0]["content"]["#text"], color=0x00ff00)
                 await self.bot.get_channel("1114354017477345330").send(embed=embed)
-                with open("/data/data.json", "w") as f:
+                with open("/data/data.json", "w") as f: # idを保存
                     json_data = json.loads(f)
                     json_data["id"] = data["entry"][0]["id"]
             else:
